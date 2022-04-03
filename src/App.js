91a8843296e90d385662bddc2798b0ee18d6fd05
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2022-04-03 13:21:36 -0400

    working first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const end = new Date(2022, 4, 16)
+  const [counter, setCounter] = useState(end - new Date()) 
+
+  useEffect(() => {
+
+    setInterval(() => {
+      setCounter(end - new Date())
+    }, 1000)
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="title">Final Countdown</div>
+      <div> Finals start in... </div>
+      <div class="countdown">
+      <>{Math.floor(counter / (1000 * 60 * 60 * 24))} days, </>
+      <>{Math.floor(counter % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))} hours, </>
+      <>{Math.floor(counter % (1000 * 60 * 60) / (1000 * 60))} minutes, and </>
+      <>{Math.floor(counter % (1000 * 60) / 1000)} seconds </>
+      </div>
+    </>
   );
 }
 
